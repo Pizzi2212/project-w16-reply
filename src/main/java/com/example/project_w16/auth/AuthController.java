b420a3e501e9c02f5b702307e20017511d970d93
Massimo Pizzicannella <maxpizzi2605@gmail.com>
2025-02-18 16:43:00 +0100

    commit

--- src/main/java/com/example/project_w16/auth/AuthController.java
+++ src/main/java/com/example/project_w16/auth/AuthController.java
@@ -21,7 +21,7 @@ public class AuthController {
         appUserService.registerUser(
                 registerRequest.getUsername(),
                 registerRequest.getPassword(),
-                Set.of(Role.ROLE_USER) // Assegna il ruolo di default
+                Set.of(Role.ROLE_ADMIN) // Assegna il ruolo di default
         );
         return ResponseEntity.ok("Registrazione avvenuta con successo");
     }
--- src/main/java/com/example/project_w16/dipendente/DipendenteController.java
+++ src/main/java/com/example/project_w16/dipendente/DipendenteController.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -26,6 +28,8 @@ public class DipendenteController {
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    @PreAuthorize("hasRole('ADMIN')")
     public Dipendente createDipendente(@RequestBody DipendenteRequest dipendenteRequest) {
         Dipendente dipendente = new Dipendente();
         dipendente.setUsername(dipendenteRequest.getUsername());
